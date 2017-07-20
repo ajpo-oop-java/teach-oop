0da42bd03dfe344ba18f8db51e0073332e9573a1
Unknown <claudio.sepulveda.h@gmail.com>
2017-07-20 15:08:31 -0400

    grabar primera prueba

--- teach-oop-java-rock-scissor-paper/src/ajpo/rcp/impl/Paper.java
+++ teach-oop-java-rock-scissor-paper/src/ajpo/rcp/impl/Paper.java
@@ -7,7 +7,16 @@ public class Paper implements GameRCP{
 	@Override
 	public boolean play(GameRCP v) {
 		// TODO Auto-generated method stub
-		return false;
+		ganador boolean = false;
+		
+		if (v instanceof Rock){
+			ganador = true;
+		}
+		if (v instanceof Paper) {
+		}
+		if (v instanceof Scissor) {
+		}
+		return ganador;
 	}
 
 }
